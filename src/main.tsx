289e7fb3f89974536900dd1bcb7fb11c3ceeabe2
chepeLove <ilya.chepelov@gmail.com>
2024-02-23 17:31:38 +0300

    fix: change styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from '@/App'
 import ReactDOM from 'react-dom/client'
 
 import './styles/index.scss'
+import './styles/normalize.scss'
 import '@fontsource/poppins/400.css'
 import '@fontsource/poppins/700.css'
 
