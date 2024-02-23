be3663fb238645d223d6278b9e0506d136944b8e
chepeLove <ilya.chepelov@gmail.com>
2024-02-23 15:43:36 +0300

    docs: add fonts and styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import { App } from '@/App'
 import ReactDOM from 'react-dom/client'
 
+import './styles/index.scss'
+import '@fontsource/poppins/400.css'
+import '@fontsource/poppins/700.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
