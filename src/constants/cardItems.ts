485a76aa6c1105757d66d7b29d0014ba14540b61
chepeLove <ilya.chepelov@gmail.com>
2024-02-24 19:13:44 +0300

    feat: add cardItems constant

---
+++ src/constants/cardItems.ts
@@ -0,0 +1,131 @@
+import imageItem1 from '@/assets/images/coffe-item-1.png'
+import imageItem2 from '@/assets/images/coffe-item-2.png'
+import imageItem3 from '@/assets/images/coffe-item-3.png'
+import imageItem4 from '@/assets/images/coffe-item-4.png'
+import imageItem5 from '@/assets/images/coffe-item-5.png'
+import imageItem6 from '@/assets/images/coffe-item-6.png'
+import imageItem7 from '@/assets/images/coffe-item-7.png'
+import imageItem8 from '@/assets/images/coffe-item-8.png'
+import imageItem9 from '@/assets/images/coffe-item-9.png'
+export interface ICardItem {
+  compound: string
+  gost: string
+  id: string
+  image: string
+  storageConditions: string
+  subtitle: string
+  term: string
+  title: string
+  weight: string
+}
+
+export const cardItems: ICardItem[] = [
+  {
+    compound: 'кофе натуральный жареный в зернах, средней обжарки',
+    gost: '32775-2014',
+    id: '1',
+    image: imageItem1,
+    storageConditions:
+      'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
+    subtitle: '100% Арабика',
+    term: '18 месяцев',
+    title: 'REY NEGRO ARABICA',
+    weight: '1000 гр',
+  },
+  {
+    compound: 'кофе натуральный жареный в зернах, средней обжарки',
+    gost: '32775-2014',
+    id: '2',
+    image: imageItem2,
+    storageConditions:
+      'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
+    subtitle: 'Робуста',
+    term: '18 месяцев',
+    title: 'REY NEGRO BARISTA',
+    weight: '1000 гр',
+  },
+  {
+    compound: 'кофе натуральный жареный в зернах, средней обжарки',
+    gost: '32775-2014',
+    id: '3',
+    image: imageItem3,
+    storageConditions:
+      'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
+    subtitle: 'Арабика 60% / Робуста 40%',
+    term: '18 месяцев',
+    title: 'REY NEGRO DE LUX',
+    weight: '1000 гр',
+  },
+  {
+    compound: 'кофе натуральный жареный в зернах, средней обжарки',
+    gost: '32775-2014',
+    id: '4',
+    image: imageItem4,
+    storageConditions:
+      'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
+    subtitle: '100% Арабика',
+    term: '18 месяцев',
+    title: 'REY NEGRO ARABICA',
+    weight: '1000 гр',
+  },
+  {
+    compound: 'кофе натуральный жареный в зернах, средней обжарки',
+    gost: '32775-2014',
+    id: '5',
+    image: imageItem5,
+    storageConditions:
+      'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
+    subtitle: '100% Арабика',
+    term: '18 месяцев',
+    title: 'REY NEGRO ARABICA',
+    weight: '1000 гр',
+  },
+  {
+    compound: 'кофе натуральный жареный в зернах, средней обжарки',
+    gost: '32775-2014',
+    id: '6',
+    image: imageItem6,
+    storageConditions:
+      'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
+    subtitle: '100% Арабика',
+    term: '18 месяцев',
+    title: 'REY NEGRO ARABICA',
+    weight: '1000 гр',
+  },
+  {
+    compound: 'кофе натуральный жареный в зернах, средней обжарки',
+    gost: '32775-2014',
+    id: '7',
+    image: imageItem7,
+    storageConditions:
+      'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
+    subtitle: '100% Арабика',
+    term: '18 месяцев',
+    title: 'REY NEGRO ARABICA',
+    weight: '1000 гр',
+  },
+  {
+    compound: 'кофе натуральный жареный в зернах, средней обжарки',
+    gost: '32775-2014',
+    id: '8',
+    image: imageItem8,
+    storageConditions:
+      'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
+    subtitle: '100% Арабика',
+    term: '18 месяцев',
+    title: 'REY NEGRO ARABICA',
+    weight: '1000 гр',
+  },
+  {
+    compound: 'кофе натуральный жареный в зернах, средней обжарки',
+    gost: '32775-2014',
+    id: '9',
+    image: imageItem9,
+    storageConditions:
+      'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
+    subtitle: '100% Арабика',
+    term: '18 месяцев',
+    title: 'REY NEGRO ARABICA',
+    weight: '1000 гр',
+  },
+]
