6a5d67c0d1798a24e87ec33fb1167db6161470dd
chepeLove <ilya.chepelov@gmail.com>
2024-02-24 19:16:49 +0300

    feat: add component

---
+++ src/layout/main/sections/home/Home.tsx
@@ -0,0 +1,16 @@
+import home from '@/assets/images/home.png'
+import { Button } from '@/components/button'
+import { Image } from '@/components/image'
+
+import styles from './Home.module.scss'
+
+export const Home = () => {
+  return (
+    <section className={styles.home}>
+      <Image className={styles['home-image']} url={home} />
+      <Button as={'a'} className={styles['home-button']} href={'#getPrice'}>
+        ПОЛУЧИТЬ ОПТОВЫЙ ПРАЙС
+      </Button>
+    </section>
+  )
+}
