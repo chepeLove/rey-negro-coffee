20c6fca1538437d41e66c804f5c5c2e0fb958344
chepeLove <ilya.chepelov@gmail.com>
2024-02-25 20:35:15 +0300

    feat: add AboutUs component

--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -1,3 +1,5 @@
+import { aboutUsInfo } from '@/constants/aboutUsInfo'
+import { AboutUs } from '@/layout/main/sections/aboutUs'
 import { Home } from '@/layout/main/sections/home'
 import { CoffeeCards } from '@/layout/main/sections/сoffeeCards'
 
@@ -6,6 +8,11 @@ export const Main = () => {
     <main>
       <Home />
       <CoffeeCards endIndex={3} />
+      <AboutUs {...aboutUsInfo[0]} />
+      <CoffeeCards endIndex={6} startIndex={3} />
+      <AboutUs {...aboutUsInfo[1]} containerPosition={'left'} isShowContactUsButton />
+      <CoffeeCards startIndex={6} />
+      <AboutUs {...aboutUsInfo[2]} containerPosition={'right'} />
     </main>
   )
 }
