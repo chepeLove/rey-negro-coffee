5797491afe68f412f1371a960aae6936aaeb6e21
chepeLove <ilya.chepelov@gmail.com>
2024-02-26 21:38:44 +0300

    feat: add logo

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,6 +1,6 @@
+import { Logo } from '@/assets/icons'
 import { Container } from '@/components/container/Container'
 import { Menu } from '@/components/menu'
-import { Typography } from '@/components/typography'
 
 import styles from './Header.module.scss'
 
@@ -8,9 +8,7 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <Container className={styles['header-container']}>
-        <Typography as={'h1'} className={styles['header-name']} variant={'h1'}>
-          REY NEGRO
-        </Typography>
+        <Logo height={'48'} width={'230'} />
         <Menu />
       </Container>
     </header>
