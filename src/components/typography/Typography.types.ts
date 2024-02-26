f511514e62be3b76e5a8829c161d50c94a95369b
chepeLove <ilya.chepelov@gmail.com>
2024-02-26 20:19:01 +0300

    feat: add new variant

--- src/components/typography/Typography.types.ts
+++ src/components/typography/Typography.types.ts
@@ -1,6 +1,6 @@
 import { ElementType, ReactNode } from 'react'
 
-type VariantType = 'bold' | 'h1' | 'h2' | 'h3' | 'regular'
+type VariantType = 'bold' | 'h1' | 'h2' | 'h3' | 'regularBig' | 'regularSmall'
 
 export interface ITypographyProps<T extends ElementType> {
   as?: T
