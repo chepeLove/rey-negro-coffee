1431af7e65c443ed94e1f1dc9e20c372df2fe9bb
chepeLove <ilya.chepelov@gmail.com>
2024-02-26 21:38:11 +0300

    feat: add component

---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { Logo } from '@/assets/icons'
+
+import styles from './Footer.module.scss'
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <Logo />
+    </footer>
+  )
+}
