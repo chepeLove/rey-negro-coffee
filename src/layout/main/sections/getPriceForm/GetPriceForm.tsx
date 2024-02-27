2d0d3b3ce41c172e96c5cb0baf29d76f4168e180
chepeLove <ilya.chepelov@gmail.com>
2024-02-27 17:42:01 +0300

    feat: add handler for the form

--- src/layout/main/sections/getPriceForm/GetPriceForm.tsx
+++ src/layout/main/sections/getPriceForm/GetPriceForm.tsx
@@ -1,3 +1,5 @@
+import { FormEvent } from 'react'
+
 import coffeeImageLeft from '@/assets/images/get-price-form-1.png'
 import coffeeImageRight from '@/assets/images/get-price-form-2.png'
 import { Button } from '@/components/button'
@@ -9,8 +11,18 @@ import { Typography } from '@/components/typography'
 import styles from './GetPriceForm.module.scss'
 
 export const GetPriceForm = () => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const name = formData.get('name')
+    const email = formData.get('email')
+    const phone = formData.get('phone')
+
+    console.info('Отправленные данные:', email, name, phone)
+  }
+
   return (
-    <section className={styles.getPriceForm}>
+    <section className={styles.getPriceForm} id={'getPrice'}>
       <Container className={styles['getPriceForm-container']} direction={'column'}>
         <Image className={styles['getPriceForm-container-image-left']} url={coffeeImageLeft} />
         <Image className={styles['getPriceForm-container-image-right']} url={coffeeImageRight} />
@@ -19,21 +31,21 @@ export const GetPriceForm = () => {
           <Typography as={'span'} variant={'h1'}>
             {'ОПТОВЫЙ ПРАЙС'}
           </Typography>
-          {' ПРЯМО СЕЙЧА'}
+          {' ПРЯМО СЕЙЧАС!'}
         </Typography>
         <Container className={styles['getPriceForm-content-box']}>
           <Container className={styles['getPriceForm-content-box-text']} direction={'column'}>
             <Typography as={'h2'} variant={'h2'}>
-              ПРЕДЛОЖЕНИЕ ДЛЯ КОРПОРАТИВНЫХ КЛИЕНТОВ
+              {'ПРЕДЛОЖЕНИЕ ' + ' ДЛЯ КОРПОРАТИВНЫХ' + ' КЛИЕНТОВ'}
             </Typography>
             <Typography variant={'regularBig'}>
               Индивидуальные условия! Для бизнеса, для офисов, ресторанов, кофеен, отелей
             </Typography>
           </Container>
-          <form className={styles['getPriceForm-form']}>
-            <TextField placeholder={'имя'} />
-            <TextField placeholder={'e-mail'} type={'email'} />
-            <TextField placeholder={'телефон'} type={'tel'} />
+          <form className={styles['getPriceForm-form']} onSubmit={handleSubmit}>
+            <TextField name={'name'} placeholder={'имя'} />
+            <TextField name={'email'} placeholder={'e-mail'} type={'email'} />
+            <TextField name={'phone'} placeholder={'телефон'} type={'tel'} />
             <Button fullWidth type={'submit'} variant={'secondary'}>
               ПОЛУЧИТЬ ОПТОВЫЙ ПРАЙС
             </Button>
