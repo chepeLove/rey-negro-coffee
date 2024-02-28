b184088e0ce1b2ef47bad1383518cd5e17d9534c
chepeLove <ilya.chepelov@gmail.com>
2024-02-28 16:15:24 +0300

    feat: add index

---
+++ src/assets/images/index.ts
@@ -0,0 +1,33 @@
+import aboutUs1 from './about-us-1.png'
+import aboutUs2 from './about-us-2.png'
+import aboutUs3 from './about-us-3.png'
+import coffeeItem1 from './coffee-item-1.png'
+import coffeeItem2 from './coffee-item-2.png'
+import coffeeItem3 from './coffee-item-3.png'
+import coffeeItem4 from './coffee-item-4.png'
+import coffeeItem5 from './coffee-item-5.png'
+import coffeeItem6 from './coffee-item-6.png'
+import coffeeItem7 from './coffee-item-7.png'
+import coffeeItem8 from './coffee-item-8.png'
+import coffeeItem9 from './coffee-item-9.png'
+import getPriceForm1 from './get-price-form-1.png'
+import getPriceForm2 from './get-price-form-2.png'
+import home from './home.png'
+
+export {
+  aboutUs1,
+  aboutUs2,
+  aboutUs3,
+  coffeeItem1,
+  coffeeItem2,
+  coffeeItem3,
+  coffeeItem4,
+  coffeeItem5,
+  coffeeItem6,
+  coffeeItem7,
+  coffeeItem8,
+  coffeeItem9,
+  getPriceForm1,
+  getPriceForm2,
+  home,
+}
