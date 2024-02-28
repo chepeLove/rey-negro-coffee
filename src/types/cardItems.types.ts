8d8b60f0fea332bd7888fecd2bf2d935e9c60ae5
chepeLove <ilya.chepelov@gmail.com>
2024-02-28 16:11:38 +0300

    feat: add interface

---
+++ src/types/cardItems.types.ts
@@ -0,0 +1,11 @@
+export interface ICardItem {
+  compound: string
+  gost: string
+  id: string
+  image: string
+  storageConditions: string
+  subtitle: string
+  term: string
+  title: string
+  weight: string
+}
