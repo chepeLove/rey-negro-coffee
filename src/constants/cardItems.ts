1e96e912ebf881d4fcd0a9a7393a74485f82390f
chepeLove <ilya.chepelov@gmail.com>
2024-02-28 16:11:21 +0300

    fix: change import images

--- src/constants/cardItems.ts
+++ src/constants/cardItems.ts
@@ -1,30 +1,22 @@
-import imageItem1 from '@/assets/images/coffe-item-1.png'
-import imageItem2 from '@/assets/images/coffe-item-2.png'
-import imageItem3 from '@/assets/images/coffe-item-3.png'
-import imageItem4 from '@/assets/images/coffe-item-4.png'
-import imageItem5 from '@/assets/images/coffe-item-5.png'
-import imageItem6 from '@/assets/images/coffe-item-6.png'
-import imageItem7 from '@/assets/images/coffe-item-7.png'
-import imageItem8 from '@/assets/images/coffe-item-8.png'
-import imageItem9 from '@/assets/images/coffe-item-9.png'
-export interface ICardItem {
-  compound: string
-  gost: string
-  id: string
-  image: string
-  storageConditions: string
-  subtitle: string
-  term: string
-  title: string
-  weight: string
-}
+import {
+  coffeeItem1,
+  coffeeItem2,
+  coffeeItem3,
+  coffeeItem4,
+  coffeeItem5,
+  coffeeItem6,
+  coffeeItem7,
+  coffeeItem8,
+  coffeeItem9,
+} from '@/assets/images'
+import { ICardItem } from '@/types'
 
 export const cardItems: ICardItem[] = [
   {
     compound: 'кофе натуральный жареный в зернах, средней обжарки',
     gost: '32775-2014',
     id: '1',
-    image: imageItem1,
+    image: coffeeItem1,
     storageConditions:
       'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
     subtitle: '100% Арабика',
@@ -36,7 +28,7 @@ export const cardItems: ICardItem[] = [
     compound: 'кофе натуральный жареный в зернах, средней обжарки',
     gost: '32775-2014',
     id: '2',
-    image: imageItem2,
+    image: coffeeItem2,
     storageConditions:
       'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
     subtitle: 'Робуста',
@@ -48,7 +40,7 @@ export const cardItems: ICardItem[] = [
     compound: 'кофе натуральный жареный в зернах, средней обжарки',
     gost: '32775-2014',
     id: '3',
-    image: imageItem3,
+    image: coffeeItem3,
     storageConditions:
       'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
     subtitle: 'Арабика 60% / Робуста 40%',
@@ -60,7 +52,7 @@ export const cardItems: ICardItem[] = [
     compound: 'кофе натуральный жареный в зернах, средней обжарки',
     gost: '32775-2014',
     id: '4',
-    image: imageItem4,
+    image: coffeeItem4,
     storageConditions:
       'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
     subtitle: '100% Арабика',
@@ -72,7 +64,7 @@ export const cardItems: ICardItem[] = [
     compound: 'кофе натуральный жареный в зернах, средней обжарки',
     gost: '32775-2014',
     id: '5',
-    image: imageItem5,
+    image: coffeeItem5,
     storageConditions:
       'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
     subtitle: '100% Арабика',
@@ -84,7 +76,7 @@ export const cardItems: ICardItem[] = [
     compound: 'кофе натуральный жареный в зернах, средней обжарки',
     gost: '32775-2014',
     id: '6',
-    image: imageItem6,
+    image: coffeeItem6,
     storageConditions:
       'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
     subtitle: '100% Арабика',
@@ -96,7 +88,7 @@ export const cardItems: ICardItem[] = [
     compound: 'кофе натуральный жареный в зернах, средней обжарки',
     gost: '32775-2014',
     id: '7',
-    image: imageItem7,
+    image: coffeeItem7,
     storageConditions:
       'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
     subtitle: '100% Арабика',
@@ -108,7 +100,7 @@ export const cardItems: ICardItem[] = [
     compound: 'кофе натуральный жареный в зернах, средней обжарки',
     gost: '32775-2014',
     id: '8',
-    image: imageItem8,
+    image: coffeeItem8,
     storageConditions:
       'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
     subtitle: '100% Арабика',
@@ -120,7 +112,7 @@ export const cardItems: ICardItem[] = [
     compound: 'кофе натуральный жареный в зернах, средней обжарки',
     gost: '32775-2014',
     id: '9',
-    image: imageItem9,
+    image: coffeeItem9,
     storageConditions:
       'хранить в сухом прохладном месте, при температуре не выше 27 °C и относительной влажности воздуха не более 75%',
     subtitle: '100% Арабика',
